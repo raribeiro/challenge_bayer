21293781cec5b6451a797480ed6693b225a51e53
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2022-06-08 00:13:47 -0300

    Update open and close test

--- java_project/src/br/com/fiap/executavel/TestConnectionManager.java
+++ java_project/src/br/com/fiap/executavel/TestConnectionManager.java
@@ -1,13 +1,16 @@
 package br.com.fiap.executavel;
 
+import java.sql.SQLException;
+
 import br.com.fiap.connection.ConnectionManager;
 
 public class TestConnectionManager {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		ConnectionManager conexao = new ConnectionManager();
 
 		conexao.connect();
+		conexao.close();
 	}
 
 }
